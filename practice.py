30b193774ed89a23da442f9b0bb8c8a89dab729c
Eliseo Duque <duque.eliseo.7@gmail.com>
2025-01-26 23:52:21 -0600

    Create practice.py

---
+++ practice.py
@@ -0,0 +1,91 @@
+# reviewing my python skills after a long 4 years
+
+
+#input() #returns string by default
+# cast input() using float() or int() constructor
+
+l = k = j = 1
+# is equivilent to
+j = 1
+k = j
+l = k
+
+#swaping values can be done like this
+#this helps avoid creating a temporary value, which comes much in handy
+j, k = k, j
+
+
+## python operands
+
+a = 3 + 7
+a //= 2
+print( a / 2 )  # float division. aka TRUE (realistic) division
+print( a // 2)  # int division.   aka FLOOT (truncated) division
+
+# tip - make numbers more readable using underscores!
+
+a = 234234_234234.2122222_33333
+print(a)
+
+# exponentiation
+a = 5
+a ** 3
+print(a)
+
+
+
+#########
+#########
+#########
+#########
+#########
+#########
+
+import random
+
+b = random.randint(4, 8) #inclusive
+print(b)
+
+b = random.randrange(4,8) #exclusive 8. generates nubmer from 4 to (8-1)
+print(b)
+
+#generate random float from 0 to 1, just like in c++
+b = random.random()
+print(b)
+
+#set the random seed using
+b = random.seed(2342)
+
+# printing using commas
+
+print(b, a, j, l, k)
+
+if j + k >= 0:
+    print("the sum of j and k are greater than 1!!!!, their sum is ", j+k)
+
+import sys
+#optional exit with exit code!!! wow
+#sys.exit(1)
+
+#conditional expressions
+# turn this
+
+if (l == 1):
+    j = 23
+else:
+    j = 0
+
+#into this
+
+#j = 2  if l == 1 else j = 0
+
+def max(n1, n2):
+    return n1 if n1 > n2 else n2
+
+print("The max value from l, k, j, a, and b is: ", max(l, max(k, max(j, max(a, b)))))
+
+# return -1 or 1 only
+
+print("random int is now ", -1 if random.randint(0,1) == 0 else 1)
+
+#switch statement equivilent for python
