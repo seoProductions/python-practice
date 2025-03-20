a66e936f17c797987933ac8ef6b84b59e22fa440
seoProductions <duque.eliseo.7@gmail.com>
2025-03-20 00:00:49 -0500

    list references and edge cases

--- basic-datastructures.py
+++ basic-datastructures.py
@@ -86,3 +86,73 @@ value = True if len(list1) < 10 else False
 
 print(f"concatonating lists are easy { list1 + list2}")
 print(f"replacating lists via multiplication operator { list1 * 4 }\n")
+
+print(list1.__contains__(3))
+
+for i in list1:
+    print(i)
+
+# just like java, lists are compared lexiographically
+
+print (list1 >= list2)
+
+
+###### Python Only!!!!!!!!!!!!!!! ######
+
+print(x for x in range(19) if x < 10) ## will print 0 thru 9
+#assign it too!
+
+newlist = [x * 3 for x in range(19) if x < 10]
+##  The way this works is 
+#
+#   python returns a sequence of x's based off of
+#   the conditions and iterations specified!
+#
+
+
+#               Programming Problem 2:
+#
+#Given list1 as a list and list2 as another list created by replicating list1 a 
+#number of times, write a statement to compute and assign it to the variable n.
+interleaved = []
+index1 = 0
+index2 = 0
+for i in range(max(len(list1), len(list2))): # total elements
+
+    if index1 < len(list1):
+        interleaved.append(list1[index1])
+
+    if index2 < len(list2):
+        interleaved.append(list2[index2])
+# will compare lexiographically
+#print(max(list1, list2))
+
+"sdflkj".split
+
+
+
+#array shifting trick, shifting left
+print( list1[1:] + list1[:1])
+#array shifting trick, shifting right
+print( [list1[-1]] + list1[:len(list1) - 1])
+
+
+import statistics
+
+print(f"average of list 1 is { statistics.mean(list1) }")
+print(f"average of list 2 is { statistics.mean(list2) }")
+
+
+def function(ls):
+    print("id INSIDE",id(ls))
+    return ls
+
+ls = [234,23,2]
+print("id ORIGINAL:", id(ls))
+
+ls23 = function(ls)
+print("id :", id(ls23))
+
+
+
+scores = list(input())
